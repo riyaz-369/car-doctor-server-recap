1fafed30151cec69a498076c058c7d98d9dab1ab
Your Nameriyazulislam99 <riyazulislam0099@gmail.com>
2024-05-05 20:19:10 +0600

    modure 61 jwt recap with car doctor

---
+++ __jwt__token__.js
@@ -0,0 +1,17 @@
+/**
+ * ----------------------------
+ *      MAKE API SECURE
+ * ----------------------------
+ * The person who should have
+ *
+ * concept:
+ * 1. assign two tokens for each person (access token, refresh token)
+ * 2. access token contains: user identification (email, role, etc). valid for a shorter duration
+ * 3. refresh token is used: to recreate an access token that was expired.
+ * 4. if refresh token is invalid then logout the user
+ */
+
+/**
+ * JWT --> json web token
+ *
+ */
